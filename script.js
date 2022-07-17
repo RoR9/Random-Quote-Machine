c3a72ac1b1b313f93fd71a7871ced01da2d37f9f
RoR9 <valeriu.verebceanu@gmail.com>
2022-07-17 22:16:07 +0300

    Avoid text and color repeat

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ const quotes=["The rarest of human qualities is consistency.","Never give up on 
 
 ]
 const authors=["Jeremy Bentham","Earl Nightingale","Mahatma Gandhi","Theodore Roosevelt","John Wooden","Lao Tzu","Alan Kay","Norman Vincent","Dalai Lama","Wayne Gretzky","George Eliot","Robert Frost","Alfred, Lord Tennyson"]
-const colors=["#FF5733","#FFC300","#DAF7A6","#900C3F","#581845","#9B0AF0"]
+const colors=["#FF5733","#FFC300","#900C3F","#581845","#9B0AF0","rgb(2,0,36)","#e66465"]
+
 
 const btnColor=document.querySelector(".btn-change")
 const text=document.querySelector(".quote_text")
@@ -27,34 +28,49 @@ function showrandom(){
     
     const random=Math.floor(Math.random()*quotes.length)
     const randomColor=Math.floor(Math.random()*colors.length)
-    if(text.textContent===quotes[random]){
+    const currentColorRgb=hexToRgb(colors[randomColor])    
+    const currentColor = window.getComputedStyle(text, null).getPropertyValue("color")
+    const textQuote = window.getComputedStyle(text, "::before").getPropertyValue("background-color")
+   
+    console.log(textQuote)
+    
+    //check if the color is already in the rgb format
+    const rgbColorFormat = () => currentColorRgb.indexOf("r") === 0 ? currentColorRgb : `rgb(${currentColorRgb[0]},${currentColorRgb[1]},${currentColorRgb[2]})`;
+    
+    
+    //Avoid same text and color
+    if(text.textContent===quotes[random] || rgbColorFormat()===currentColor.replace(/\s/g,'')){
         return showrandom()
     }
     //Text change
     text.innerHTML=quotes[random]
     textAuthor.innerHTML=`- ${authors[random]}`
    
-    
-    
     //Color change
 
-    text.style.color=colors[randomColor]
+    text.style.color = colors[randomColor]
+    text.style.setProperty("--fillColor",colors[randomColor])
     textAuthor.style.color=colors[randomColor]
-    btnColor.style.background=colors[randomColor]
+    btnColor.style.background = colors[randomColor]
+    
     document.body.style.background=colors[randomColor]
+     
     icons.forEach(icon=>icon.style.color=colors[randomColor])
+    
 
 }
 
-function showrandomClick(){
+const hexToRgb = hex =>
+    hex.indexOf("#") === 0 ?
+  
+        hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
+            , (m, r, g, b) => '#' + r + r + g + g + b + b)
+            .substring(1).match(/.{2}/g)
+            .map(x => parseInt(x, 16))
+        : hex;
     
 
 
-   
-    
-
-
-}
 
 
 
